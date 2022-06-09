75d56757f5cde9a53e89b7dd14d72912ec568eb8
Syndicalyst <denisov.dev@ukr.net>
2022-06-09 14:31:37 +0300

    Formatting some dates

--- utils.mjs
+++ utils.mjs
@@ -16,10 +16,11 @@ let formatDate = (date) => {
 export function displayWeather(bot, chatId, action) {
   let str = '';
   axios.get(url).then(function (response) {
-    let tempDate = new Date(response.data.list[0].dt_txt).toLocaleDateString('ru', { weekday:"long", month:"long", day:"numeric"});
+    let tempDate = new Date(response.data.list[0].dt_txt);
+    let tempDateString = tempDate.toLocaleDateString('ru', { weekday:"long", month:"long", day:"numeric"});
 
     str += `Погода в Киеве на сегодня: `
-    str += `\n\n${tempDate.charAt(0).toUpperCase() + tempDate.slice(1)}: `;
+    str += `\n\n${tempDateString.charAt(0).toUpperCase() + tempDateString.slice(1)}: `;
 
     (response.data.list).forEach(element => {
 
