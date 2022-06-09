f60c03344518f8c1a842c5b99c61cf7f001d6879
Syndicalyst <denisov.dev@ukr.net>
2022-06-09 14:16:14 +0300

    lenguage update

--- utils.mjs
+++ utils.mjs
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=50.4333&lon=30.5167&lang=ua&units=metric&exclude=hourly,daily&appid=4c64e890924bf1087d8af4018ace8eed'
+const url = 'https://api.openweathermap.org/data/2.5/forecast?lat=50.4333&lon=30.5167&lang=ru&units=metric&exclude=hourly,daily&appid=4c64e890924bf1087d8af4018ace8eed'
  
 let formatDate = (date) => {
 
@@ -18,21 +18,21 @@ export function displayWeather(bot, chatId, action) {
   axios.get(url).then(function (response) {
     let tempDate = new Date(response.data.list[0].dt_txt);
 
-    str += `Погода в Києві на сьогодні: `
-    str += `\n\n${tempDate.toLocaleDateString('ua', { weekday:"long", month:"long", day:"numeric"})}: `;
+    str += `Погода в Киеве на сегодня: `
+    str += `\n\n${tempDate.toLocaleDateString('ru', { weekday:"long", month:"long", day:"numeric"})}: `;
 
     (response.data.list).forEach(element => {
 
       let day = new Date(element.dt_txt);
-      let formattedDay = day.toLocaleDateString('ua', { weekday:"long", month:"long", day:"numeric"})
+      let formattedDay = day.toLocaleDateString('ru', { weekday:"long", month:"long", day:"numeric"})
       let dayTime = formatDate(new Date(element.dt_txt));
       if (day.getHours() % +action == 0) {
         if (tempDate.getYear() != day.getYear() || tempDate.getMonth() != day.getMonth() || tempDate.getDate() != day.getDate()) {
           str += (`\n\n${formattedDay}: `);
-          str += (`\n  ${dayTime} +${Math.trunc(element.main.temp)} °C відчувається як: +${Math.trunc(element.main.feels_like)} °C, ${element.weather[0].description}`);
+          str += (`\n  ${dayTime} +${Math.trunc(element.main.temp)} °C ощущается как: +${Math.trunc(element.main.feels_like)} °C, ${element.weather[0].description}`);
           tempDate = day;
         } else {
-          str += (`\n  ${dayTime} +${Math.trunc(element.main.temp)} °C відчувається як: +${Math.trunc(element.main.feels_like)} °C, ${element.weather[0].description}`);
+          str += (`\n  ${dayTime} +${Math.trunc(element.main.temp)} °C ощущается как: +${Math.trunc(element.main.feels_like)} °C, ${element.weather[0].description}`);
         }
       }
     })
